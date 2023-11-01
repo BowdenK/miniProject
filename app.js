194b8e72c270f346b73d07cb23aa3b34fff43cac
BowdenK <112501404+BowdenK@users.noreply.github.com>
2023-11-01 07:06:57 +0000

    added spin anims and changed flex

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 let keySlot = document.getElementById("key")
 let codeSlot = document.getElementById("code")
 let valueSlot = document.getElementById("value")
+let whichSlot = document.getElementById("which")
 
 
 document.addEventListener('keyup', (event) => {
     let name = event.key;
     let code = event.code;
     let value = event.key.charCodeAt(0)
+    let which = event.which;
 
     keySlot.textContent = name;
     codeSlot.textContent = code;
     valueSlot.textContent = value;
+    whichSlot.textContent = which;
 
     console.log(`Key pressed: ${name} \r\n Key code value: ${code}`);
 }, false)
